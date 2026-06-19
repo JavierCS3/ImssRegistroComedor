25c1e251f88b4865ed44f5350a2a5e8193c76983
Javier <javier.castillo239873@potros.itson.edu.mx>
2026-06-18 17:08:07 -0700

    y

--- src/main/java/com/example/imssregistrocomedor/dao/RegistroDAO.java
+++ src/main/java/com/example/imssregistrocomedor/dao/RegistroDAO.java
@@ -2,10 +2,14 @@ package com.example.imssregistrocomedor.dao;
 
 import com.example.imssregistrocomedor.conection.Conexion;
 import com.example.imssregistrocomedor.controller.Registro;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 import org.bson.Document;
+
+import java.time.LocalDateTime;
 
 
 public class RegistroDAO {
@@ -13,39 +17,31 @@ public class RegistroDAO {
 
     public void guardar(Registro r){
 
-
-        System.out.println("Entrando DAO");
-
-
-        MongoDatabase db = null;
-
-        System.out.println("LLAMANDO CONEXION");
-
-        db = Conexion.conectar();
-
-        System.out.println("REGRESO DE CONEXION");
+        MongoClient cliente =
+                MongoClients.create(
+                        "mongodb+srv://javierefrencs_db_user:J123456@imssregistro.kau1t1i.mongodb.net/comedor_imss?retryWrites=true&w=majority"
+                );
 
 
-        System.out.println("Base conectada");
+        System.out.println("Mongo creado");
+
+
+        MongoDatabase db =
+                cliente.getDatabase("comedor_imss");
+
+
+        System.out.println("DB lista");
 
 
         MongoCollection<Document> coleccion =
                 db.getCollection("asistencias");
 
 
-        System.out.println("Coleccion obtenida");
-
-
-
         Document doc =
                 new Document()
                         .append("nombre", r.getNombre())
                         .append("empleadoId", r.getEmpleadoId())
-                        .append("fecha", r.getFecha().toString());
-
-
-
-        System.out.println("Insertando...");
+                        .append("fecha", LocalDateTime.now().toString());
 
 
         coleccion.insertOne(doc);
